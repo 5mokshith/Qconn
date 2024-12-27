556439b154cb52b7346eaf217e5801036c3ed3c9
5mokshith <143961455+5mokshith@users.noreply.github.com>
2024-12-27 11:41:52 +0530

    Update animation.js

--- script/utills/animation.js
+++ script/utills/animation.js
@@ -2,6 +2,7 @@
 
 let blob = document.querySelector(".blob");
 let heroBg = document.querySelector("body .hero[data-identifier='questions']");
+let profileBg = document.querySelector("body .hero[data-identifier='profile']");
 let theme = () =>  localStorage.getItem("theme") || "light";
 setHeroBg();
 if (
@@ -17,6 +18,13 @@ export function setHeroBg() {
         heroBg.style.background = "linear-gradient(to top, #f7797d, #FBD786, #C6FFDD)";
     }else {
         heroBg.style.background = " radial-gradient(#0000ffdb, var(--toogle-btn-text) 60%)";
+    }
+    if(profileBg) {
+        if (localStorage.getItem("theme") === "light") {
+            profileBg.style.background = "";
+        }else {
+            profileBg.style.background = "radial-gradient(rgba(0, 0, 0, 0.148),#0000ff95, var(--toogle-btn-text) 80%)";
+        }
     }
 }
 
